0e64dc058df61153197269c2c92e02220e423805
Niek Kruse <niekkruse70@gmail.com>
2020-07-08 18:49:12 +0200

    feat: repro of bug

---
+++ src/apollo-2.spec.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import gql from "graphql-tag";
+import { render } from "@testing-library/react";
+import { MockedProvider } from "@apollo/react-testing";
+import { useQuery } from "@apollo/react-hooks";
+
+const SOME_GQL_QUERY = gql`
+  query viewer {
+    viewer {
+      id
+    }
+  }
+`;
+
+const useViewerQuery = () => useQuery(SOME_GQL_QUERY);
+
+const TestComponent = () => {
+  const { error, data } = useViewerQuery();
+
+  return error ? <>{`has error: ${error.message}`}</> : <>"No error."</>;
+};
+
+describe("ApolloMockProvider", () => {
+  test("returns missing mock error as query hook error.", async () => {
+    const { container } = render(
+      <MockedProvider mocks={[]}>
+        <TestComponent />
+      </MockedProvider>
+    );
+
+    // hacky way to wait for query to be resolved
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    expect(container.innerHTML).toMatchInlineSnapshot(`
+      "has error: Network error: No more mocked responses for the query: query viewer {
+        viewer {
+          id
+          __typename
+        }
+      }
+      , variables: {}"
+    `);
+  });
+});
---
+++ src/apollo-3.spec.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { render } from "@testing-library/react";
+import { gql, useQuery } from "@apollo/client";
+import { MockedProvider } from "@apollo/client/testing";
+
+const SOME_GQL_QUERY = gql`
+  query viewer {
+    viewer {
+      id
+    }
+  }
+`;
+
+const useViewerQuery = () => useQuery(SOME_GQL_QUERY);
+
+const TestComponent = () => {
+  const { error, data } = useViewerQuery();
+
+  return error ? <>{`has error: ${error.message}`}</> : <>"No error."</>;
+};
+
+describe("ApolloMockProvider", () => {
+  test("returns missing mock error as query hook error.", () => {
+    const { container } = render(
+      <MockedProvider mocks={[]}>
+        <TestComponent />
+      </MockedProvider>
+    );
+
+    // hacky way to wait for query to be resolved
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    expect(container.innerHTML).toMatchInlineSnapshot(`
+      "has error: Network error: No more mocked responses for the query: query viewer {
+        viewer {
+          id
+          __typename
+        }
+      }
+      , variables: {}"
+    `);
+  });
+});
